fb8fea90638fbacc8bef93a9224c2c9c999fd3a3
lucas <lucasleproust@gmail.com>
2020-11-14 23:41:44 +0100

    remove route from app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UsersComponent } from "./components/users/users.component";
-import { AddUserComponent } from "./components/add-user/add-user.component";
 
 const routes: Routes = [
-  {path:'', component: UsersComponent},
-  {path:'test', component: AddUserComponent}
 ];
 
 @NgModule({
