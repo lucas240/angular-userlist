ddd49a486392dc9d4602237caa6c6a140c8a6490
lucas <lucasleproust@gmail.com>
2020-11-15 02:20:53 +0100

    add nav in header and change style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from "./components/users/users.component";
+import { TableUserComponent } from "./components/table-user/table-user.component";
 
 const routes: Routes = [
+  {path:'', component: UsersComponent},
+  {path:'table', component: TableUserComponent}
 ];
 
 @NgModule({
