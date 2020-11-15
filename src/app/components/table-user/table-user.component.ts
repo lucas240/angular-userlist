773b81d2048c60df8311422cbd2701a0b2210c68
lucas <lucasleproust@gmail.com>
2020-11-15 02:21:29 +0100

    create table component who list users

---
+++ src/app/components/table-user/table-user.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { UserModel } from "../../models/UserModel";
+import { UserService } from "../../services/user.service";
+import { MatTable } from '@angular/material/table';
+
+@Component({
+  selector: 'app-table-user',
+  templateUrl: './table-user.component.html',
+  styleUrls: ['./table-user.component.css']
+})
+export class TableUserComponent implements OnInit {
+  @Input() user: UserModel;
+  @ViewChild(MatTable) table: MatTable<any>;
+
+  users:UserModel[];
+  displayedColumns: string[] = [
+    'id', 
+    'name', 
+    'username', 
+    'email', 
+    'action'
+  ];
+
+  constructor(private userService:UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(users=>{
+      this.users = users
+    })
+  }
+
+  onDelete(user){
+    // delete from UI
+    this.users = this.users.filter(u => u.id != user.id)
+    // delete from server
+    this.userService.deleteUser(user).subscribe()
+  }
+}
